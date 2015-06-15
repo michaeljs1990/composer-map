d1c259a39ec57347afee1df3701c795f3b0a82fc
Michael Schuett <michaeljs1990@gmail.com>
2015-06-15 00:37:15 -0400

    Alter structure

--- cmap.php
+++ cmap.php
@@ -1,7 +1,6 @@
-#!/usr/bin/env php
 <?php
 
-require "vendor/autoload.php";
+require __DIR__ . "/vendor/autoload.php";
 
 use Michaeljs1990\Cmap\CmapCommand;
 use Symfony\Component\Console\Application;
